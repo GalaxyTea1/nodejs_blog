9b0a73ff55678a0f0764d76dbf93091e963d25a5
Trong Dang <trong_1751220006@dau.edu.vn>
2021-09-17 13:38:44 +0700

    up

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// XMLHttpRequest, fetch, axios,..
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -19,11 +25,20 @@ app.set('views', path.join(__dirname, 'resources\\views'));
 
 app.get('/trang-chu', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
 
 // 127.0.0.1 - localhost
 
