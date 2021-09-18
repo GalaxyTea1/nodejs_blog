b9890c2203912e0335952457ea6739d568486e7f
Trong Dang <trong_1751220006@dau.edu.vn>
2021-09-18 12:15:21 +0700

    up

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -23,22 +25,9 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/trang-chu', (req, res) => {
-  res.render('home');
-});
+// Route init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
-});
 
 // 127.0.0.1 - localhost
 
